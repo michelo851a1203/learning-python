2381a9d011edd700df29fb17acbe8ef5c0ca71a5
MichaelHo <michelo851a1203@gmail.com>
2023-06-26 09:17:07 +0800

    init

---
+++ learning_lstrip.py
@@ -0,0 +1,9 @@
+links = [
+        "www.b001.io",
+        "www.youtube.com",
+        "www.google.com",
+        "www.wikipedia.org"
+        ]
+
+for link in links:
+    print(link.lstrip("www."))
